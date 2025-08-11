d3ba35684d95e67699c82b60e9c83669fe2c5a8a
Samuel <samuelolaseinde14@gmail.com>
2025-08-11 10:31:24 +0100

    implement rate limiting for API requests in app and auth routes

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ const app = express();
 const helmet = require("helmet");
 const cors = require("cors");
 const { xss } = require("express-xss-sanitizer");
+const { rateLimit } = require("express-rate-limit");
 
 const notFound = require("./middleware/notFound");
 const taskRouter = require("./routes/task");
@@ -15,6 +16,17 @@ const authMiddleware = require("./middleware/authmiddleware");
 const adminRouter = require("./routes/admin");
 
 const port = process.env.PORT || 3000;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+app.use(limiter);
 
 app.use(
   cors({
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const { register, login } = require("../controllers/auth");
+const { rateLimit } = require("express-rate-limit");
 const upload = require("../middleware/upload");
 
 const router = express.Router();
 
-router.route("/register").post(upload.single("image"), register);
-router.route("/login").post(login);
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  limit: 5, // Allow only 5 attempts
+  message: { error: "Too many attempts, please try again later" },
+});
+
+router.route("/register").post(authLimiter, upload.single("image"), register);
+router.route("/login").post(authLimiter, login);
 
 module.exports = router;
